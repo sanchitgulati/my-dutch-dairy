15644e77e72aaaef7d8735265fd9b6fc43270bd6
Sanchit Gulati <sanchit.gulati@gmail.com>
2023-09-25 13:54:34 +0200

    Update calender.dart

--- lib/calender.dart
+++ lib/calender.dart
@@ -1,23 +1,77 @@
 import 'package:flutter/material.dart';
 import 'horizontal_week_calendar.dart';
+import 'package:intl/intl.dart';
 
 class Calender extends StatefulWidget {
   const Calender({super.key});
 
   @override
-  State<StatefulWidget> createState() => _CalenderState();
+  State<Calender> createState() => _HorizontalWeekCalendarPackageState();
 }
 
-class _CalenderState extends State<Calender> {
-  DateTime selectedDate = DateTime.now();
+class _HorizontalWeekCalendarPackageState extends State<Calender> {
+  var selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
-    return HorizontalWeekCalendar(
-      onDateChange: (date) {
-        setState(() {
-          selectedDate = date;
-        });
-      },
+    var theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: const Text(
+          "Horizontal Week Calendar",
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              HorizontalWeekCalendar(
+                weekStartFrom: WeekStartFrom.Monday,
+                activeBackgroundColor: Colors.purple,
+                activeTextColor: Colors.white,
+                inactiveBackgroundColor: Colors.purple.withOpacity(.3),
+                inactiveTextColor: Colors.white,
+                disabledTextColor: Colors.grey,
+                disabledBackgroundColor: Colors.grey.withOpacity(.3),
+                activeNavigatorColor: Colors.purple,
+                inactiveNavigatorColor: Colors.grey,
+                monthColor: Colors.purple,
+                onDateChange: (date) {
+                  setState(() {
+                    selectedDate = date;
+                  });
+                },
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Selected Date",
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.titleMedium!.copyWith(
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                    const SizedBox(height: 3),
+                    Text(
+                      DateFormat('dd MMM yyyy').format(selectedDate),
+                      textAlign: TextAlign.center,
+                      style: theme.textTheme.titleLarge!.copyWith(
+                        color: theme.primaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
